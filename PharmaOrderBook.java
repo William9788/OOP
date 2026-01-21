6da92aa0041834ef8b408ee4dc39f85bd4c5c342
William9788 <hewohui9788@icloud.com>
2026-01-21 23:13:47 +0000

    feat:PharmaOrderBook completed

--- PharmaOrderBook.java
+++ PharmaOrderBook.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
 public class PharmaOrderBook {
-    
+    private ArrayList<PharmaOrder> orders;
+    public PharmaOrderBook(){
+        orders = new ArrayList();
+    }
+    public boolean add(PharmaOrder order){
+        if(order==null){
+            return false;
+        }
+        orders.add(order);
+        return true;
+    }
+    public boolean add(ArrayList<PharmaOrder> orderList){
+        if(orderList== null||orderList.isEmpty()){
+            return false;
+        }
+        orders.addAll(orderList);
+        return true;
+    }
+    @Override
+    public String toString(){
+        return "PharmaOrderBook contains"+orders.size()+"order.";
+    }
 }
