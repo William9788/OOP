a9db35139ed584b5e8d306e7c0bfcabc87f9161d
William9788 <hewohui9788@icloud.com>
2026-01-22 11:59:17 +0000

    feat:addDelivery() completed

--- PharmaDeliveries.java
+++ PharmaDeliveries.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
 public class PharmaDeliveries {
-    
+    private ArrayList<MedicineDelivery> isComing;
+public PharmaDeliveries(){
+    isComing = new ArrayList<>();
+}
+public boolean addDeliveries(MedicineDelivery delivery){
+    if(delivery==null){
+        return false;
+    }
+    isComing.add(delivery);
+    return true;
+}
+
 }
